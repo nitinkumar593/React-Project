d3311e89efdd825c29bd6ff6839653e0a23aa135
Nitin Kumar <nitinkumar.in593@gmail.com>
2026-06-19 09:53:56 +0530

    implement search component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import Search from './Components/Search/Search';
 
 
 function App() {
 
   return (
     <>
-
+    <Search/>
     </>
   )
 }
---
+++ src/Components/Search/Search.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import './Search.css';
+
+export default function Search() {
+    let[city, setCity] = useState('');
+
+    let handleInput = (e) => {
+         setCity(e.target.value);
+    }
+
+    let handleSubmit = (e) => {
+         e.preventDefault();
+         console.log(city);
+         setCity("");
+    }
+
+    return (
+        <div className='SearchBox'>
+            <h3>Search for the  weather</h3>
+            <form onSubmit={handleSubmit}>
+                <TextField id="outlined-basic" label="City Name" variant="outlined" required value={city} onChange={handleInput}/>
+                <br /><br />
+                <Button variant="contained" type='submit'>
+                    Search
+                </Button>
+            </form>
+        </div>
+    )
+}
